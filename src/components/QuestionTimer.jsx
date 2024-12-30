cb26c3b99db85c3deaa5ddb157432678da76f441
Krushangpatel2610 <krushang2610.edu@gmail.com>
2024-12-30 15:01:34 +0530

    `Refactor QuestionTimer component and related changes in Quiz component`

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -1,27 +1,34 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
-export default function QuestionTimer({ timeout, onTimeout }) {
-   const [remainingTime, setRemainingTime] = useState(timeout);
-   
-   useEffect(() => {
+export default function QuestionTimer({ timeout, onTimeout, mode }) {
+  const [remainingTime, setRemainingTime] = useState(timeout);
+
+  useEffect(() => {
     console.log('SETTING TIMEOUT');
     const timer = setTimeout(onTimeout, timeout);
-    
-    return () => {
-        clearTimeout(timer);
-    };
-   }, [timeout, onTimeout]);
 
-   useEffect(() => {
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [timeout, onTimeout]);
+
+  useEffect(() => {
     console.log('SETTING INTERVAL');
     const interval = setInterval(() => {
-        setRemainingTime(prevRemainingTime => prevRemainingTime - 100);
-    }, 100);    
-  
-    return () => {
-        clearInterval(interval);
-    };
-   }, []);
+      setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
+    }, 100);
 
-   return <progress id="question-time" max={timeout} value={remainingTime} />;
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <progress
+      id="question-time"
+      max={timeout}
+      value={remainingTime}
+      className={mode}
+    />
+  );
 }
